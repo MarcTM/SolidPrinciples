d2736c945ce2fe46ce0eca7ad836b6b0c0d8654a
CORP\marctorr <marctorresmartinez@gmail.com>
2021-12-21 17:54:57 +0100

    Single responsability principle implemented closes #1

---
+++ SolidPrinciples/SingleResponsability/Correct/CocheDB.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolidPrinciples.SingleResponsability.Correct
+{
+    class CocheDB
+    {
+        void guardarCocheDB(Coche coche)
+        {
+        }
+
+        void eliminarCocheDB(Coche coche)
+        {
+        }
+    }
+}

e7ef2a33463f0eec068526fa3c67e4c899a8a59f
CORP\marctorr <marctorresmartinez@gmail.com>
2021-12-22 16:22:43 +0100

    Liskov Substitution Example correct

---
+++ SolidPrinciples/LiskovSubstitution/Correct/Principal.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolidPrinciples.LiskovSubstitution.Correct
+{
+    public class Principal
+    {
+        public static void main(String[] args)
+        {
+            Coche[] arrayCoches = {
+                new Renault(),
+                new Audi(),
+                new Mercedes()
+            };
+            imprimirNumAsientos(arrayCoches);
+            imprimirPrecioMedioCoche(arrayCoches);
+        }
+
+        public static void imprimirNumAsientos(Coche[] arrayCoches)
+        {
+            foreach (Coche coche in arrayCoches)
+            {
+                Console.WriteLine(coche.numAsientos());
+            }
+        }
+
+        public static void imprimirPrecioMedioCoche(Coche[] arrayCoches)
+        {
+            foreach (Coche coche in arrayCoches)
+            {
+                Console.WriteLine(coche.precioMedioCoche());
+            }
+        }
+    }
+}
